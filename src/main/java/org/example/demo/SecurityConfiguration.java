90d6331bcee0867543d80462b049a4f95739fdd9
Leroy <leroy@embrace.co.za>
2021-05-20 10:53:33 +0200

    feat: security on all endpoints excluding h2 console

---
+++ src/main/java/org/example/demo/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package org.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+}
