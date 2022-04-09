d39ff1abf960b32ec456938492b5fee399fe66bc
Dunn <leroy.c.dunn@gmail.com>
2022-04-09 14:42:09 +0200

    feat: advanced dynamic entity queries

--- src/main/java/org/example/demo/PersonQuery.java
+++ src/main/java/org/example/demo/PersonQuery.java
@@ -6,6 +6,10 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class PersonQuery {
+    private Integer[] ids;
     private String firstName;
+    private String firstNameGreaterThan;
     private String lastName;
+    private String lastNameIgnoreCase;
+    private String lastNameLike;
 }
--- src/main/java/org/example/demo/PersonSpecifications.java
+++ src/main/java/org/example/demo/PersonSpecifications.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // https://stackoverflow.com/a/69909639/5771199
@@ -13,12 +14,43 @@ public class PersonSpecifications {
         return (root, criteriaQuery, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
+            if (query.getIds() != null) {
+                predicates.add(root.get("id").in(Arrays.asList(query.getIds())));
+            }
+
             if (query.getFirstName() != null) {
-                predicates.add(criteriaBuilder.equal(root.get("firstName"), query.getFirstName()));
+                predicates.add(criteriaBuilder.equal(
+                        root.get("firstName"),
+                        query.getFirstName()
+                ));
+            }
+
+            if (query.getFirstNameGreaterThan() != null) {
+                predicates.add(criteriaBuilder.greaterThan(
+                        root.get("firstName"),
+                        query.getFirstNameGreaterThan()
+                ));
             }
 
             if (query.getLastName() != null) {
-                predicates.add(criteriaBuilder.equal(root.get("lastName"), query.getLastName()));
+                predicates.add(criteriaBuilder.equal(
+                        root.get("lastName"),
+                        query.getLastName()
+                ));
+            }
+
+            if (query.getLastNameIgnoreCase() != null) {
+                predicates.add(criteriaBuilder.equal(
+                        criteriaBuilder.lower(root.get("lastName")),
+                        query.getLastNameIgnoreCase().toLowerCase()
+                ));
+            }
+
+            if (query.getLastNameLike() != null) {
+                predicates.add(criteriaBuilder.like(
+                        root.get("lastName"),
+                        "%" + query.getLastNameLike() + "%"
+                ));
             }
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
